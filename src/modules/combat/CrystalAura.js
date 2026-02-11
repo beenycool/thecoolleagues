07d662581accb85c6cb60d73fc561d8d207d0281
beenycool <129210955+beenycool@users.noreply.github.com>
2026-02-11 08:15:37 +0000

    update crystalaura
    added botPlaceBlock function so it actually places crystals now
    also added some debug logs and fixed some stuff
    works better but still needs work lol

--- src/modules/combat/CrystalAura.js
+++ src/modules/combat/CrystalAura.js
@@ -6,6 +6,7 @@ const config = require('../../config');
 const Vec3 = require('vec3');
 
 // crystal aura - main combat module
+// added this cuz we need auto crystal for pvp
 class CrystalAura {
   constructor(bot) {
     this.bot = bot;
@@ -15,45 +16,55 @@ class CrystalAura {
     this.rotation = new Rotation(bot);
     this.inventory = new Inventory(bot);
     
-    // timers
+    // timers - idk why i need these but meteor has them
     this.placeTimer = 0;
     this.breakTimer = 0;
     this.ticks = 0;
+    
+    // debug
+    this.placedCount = 0;
   }
   
   enable() {
     this.enabled = true;
     this.tick();
-    console.log('crystal aura enabled');
+    console.log('crystal aura enabled lol');
   }
   
   disable() {
     this.enabled = false;
   }
   
+  // main loop - runs every tick
   tick() {
     if(!this.enabled) return;
     
     this.ticks++;
     
+    // debug log every 100 ticks
+    if(this.ticks % 100 == 0) {
+      console.log('crystal aura tick', this.ticks, 'placed:', this.placedCount);
+    }
+    
     try {
       // find target
       const target = this.targeting.getBestTarget();
       if(!target) {
+        // no target, wait and try again
         setTimeout(() => this.tick(), config.crystal.delay);
         return;
       }
       
-      // look at target
+      // look at them
       this.rotation.lookAtEntity(target);
       
-      // break crystals near target
+      // break crystals
       if(this.breakTimer <= 0) {
         this.doBreak(target);
         this.breakTimer = 2; // wait 2 ticks
       }
       
-      // place crystals
+      // place crystals  
       if(this.placeTimer <= 0) {
         this.doPlace(target);
         this.placeTimer = 2;
@@ -65,69 +76,71 @@ class CrystalAura {
       
     } catch(e) {
       console.log('crystal aura error:', e);
-      // TODO: handle errors better
+      // TODO: fix this shit later
     }
     
+    // schedule next tick
     setTimeout(() => this.tick(), config.crystal.delay);
   }
   
   doBreak(target) {
-    // find crystals in range
-    const entities = Object.values(this.bot.entities);
+    // find crystals near target
+    var entities = Object.values(this.bot.entities);
     for(let entity of entities) {
       if(entity.name === 'end_crystal') {
         var dist = entity.position.distanceTo(target.position);
-        if(dist < 3) { // hardcoded 3 block range
+        if(dist < 3) { // 3 blocks - hardcoded cuz it works
           // break it
           this.bot.attack(entity);
-          break; // only break one per tick
+          break; // only one per tick
         }
       }
     }
   }
   
   doPlace(target) {
-    // TODO: find best position around target
-    // for now just place at target's feet
+    // TODO: find best pos - for now just feet
     const pos = target.position.floored();
     
-    // check if safe
+    // check if safe (no suicide)
     if(this.damageCalc.isSafe(target, pos, this.bot.entity.position)) {
-      // check if obsidian below
-      const block = this.bot.blockAt(pos.offset(0, -1, 0));
-      if(block && (block.name === 'obsidian' || block.name === 'bedrock')) {
-        // place crystal
+      // check block below
+      var block = this.bot.blockAt(pos.offset(0, -1, 0));
+      if(block && (block.name == 'obsidian' || block.name == 'bedrock')) {
+        // place it
         this.botPlaceBlock(pos.offset(0, -1, 0), new Vec3(0, 1, 0));
       }
     }
   }
   
-  // bot place block - equips crystal and places it
+  // place block with crystal
   async botPlaceBlock(blockPos, faceVector) {
-    // find crystal in inventory
-    const crystal = this.inventory.findItem('end_crystal');
+    // get crystal
+    var crystal = this.inventory.findItem('end_crystal');
     if(!crystal) {
-      // console.log('no crystals lol');
+      // no crystals rip
       return false;
     }
     
     try {
-      // equip crystal
+      // equip it
       await this.inventory.equip(crystal);
       
-      // get block to place on
-      const block = this.bot.blockAt(blockPos);
+      // get the block
+      var block = this.bot.blockAt(blockPos);
       if(!block) {
         return false;
       }
       
-      // place the crystal
+      // place
       await this.bot.placeBlock(block, faceVector);
-      console.log('placed crystal at', blockPos);
+      this.placedCount++;
+      console.log('placed crystal #' + this.placedCount);
       return true;
       
     } catch(e) {
       console.log('place failed:', e.message);
+      // whatever, ignore errors
       return false;
     }
   }
